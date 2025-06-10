9dd3c7e43a24fd913d504befe6a462eb1b6f4fc1
Renan Rodrigues <renanfeliperodrigues.rodrigues@gmail.com>
2025-06-10 16:18:47 -0300

    Update Main.cpp & ConnectDB

--- Main.cpp
+++ Main.cpp
@@ -1,8 +1,21 @@
 #include <iostream>
 #include "menu.hpp"
 #include "ajuda.hpp"
+#include "connectDB.h" // Inclua o cabeçalho do banco de dados
+
 using namespace std;
+
 int main() {
+    // Instancia e testa a conexão com o banco de dados
+    try {
+        ServerDBConnection dbConnection;
+        // Se chegou aqui, a conexão foi bem-sucedida
+        cout << "Conexão com o banco de dados estabelecida com sucesso!" << endl;
+    } catch (...) {
+        cout << "Falha ao conectar ao banco de dados." << endl;
+        return 1; // Retorna código de erro
+    }
+
     Menu menu;
     menu.mostrarMenu();
     return 0;
